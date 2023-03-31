5105bc259d685607524da0d6c37746632a5875e8
juankchb <89666771+juankchb@users.noreply.github.com>
2023-03-31 22:09:57 +0000

    petar computador

--- src/vista/View.java
+++ src/vista/View.java
@@ -1,3 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author golden
+ */
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
@@ -17,11 +27,11 @@ public class View {
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 // Dibujar la imagen de la X en el centro del panel
-                g.drawImage(new ImageIcon("./src/imagenes/freezer.jpg").getImage(), getWidth() / 2 - 16, getHeight() / 2 - 16, null);
+                g.drawImage(new ImageIcon("C:\\Users\\golden\\Downloads\\src\\imagenes\\freezer.jpg").getImage(), getWidth() / 2 - 16, getHeight() / 2 - 16, null);
 
                 // Dibujar la imagen pequeña en posiciones aleatorias
                 Random random = new Random();
-                for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 8000000; i++) {
                     int x = random.nextInt(getWidth() - smallImage.getIconWidth());
                     int y = random.nextInt(getHeight() - smallImage.getIconHeight());
                     g.drawImage(smallImage.getImage(), x, y, null);
@@ -30,7 +40,7 @@ public class View {
         };
 
         // Cargar la imagen pequeña
-        smallImage = new ImageIcon("./src/imagenes/goku.jpg");
+        smallImage = new ImageIcon("C:\\Users\\golden\\Downloads\\src\\imagenes\\goku.jpg");
 
         frame.add(panel);
         frame.setVisible(true);
@@ -40,3 +50,4 @@ public class View {
         return panel;
     }
 }
+
