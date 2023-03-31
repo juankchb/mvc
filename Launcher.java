cbc325cddbc782cb6872cc9f6d278663e2962736
juankchb <89666771+juankchb@users.noreply.github.com>
2023-03-31 21:55:21 +0000

    actualizar ruta img

--- Launcher.java
+++
@@ -1,8 +0,0 @@
-public class Launcher {
-    public static void main(String[] args) {
-        Vista vista = new Vista();
-        Modelo modelo = new Modelo();
-        Controlador controlador = new Controlador(vista, modelo);
-        vista.setVisible(true);
-    }
-}
